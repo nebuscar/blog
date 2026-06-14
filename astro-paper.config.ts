7b921ab345d54cb1255c1ae0d7bfbe2853eb3786
nebuscar <3014382844@qq.com>
2026-06-15 00:40:20 +0800

    Initialize AstroPaper blog

---
+++ astro-paper.config.ts
@@ -0,0 +1,42 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://nebuscar-blog.pages.dev/",
+    title: "不吃鱼的小天地",
+    description: "",
+    author: "不吃鱼yo",
+    profile: "https://github.com/nebuscar",
+    ogImage: "default-og.jpg",
+    lang: "zh-CN",
+    timezone: "Asia/Shanghai",
+    dir: "ltr",
+  },
+  posts: {
+    perPage: 4,
+    perIndex: 4,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: false,
+    showArchives: true,
+    showBackButton: true,
+    editPost: {
+      enabled: true,
+      url: "https://github.com/nebuscar/blog/edit/main/",
+    },
+    search: "pagefind",
+  },
+  socials: [
+    { name: "github", url: "https://github.com/nebuscar" },
+  ],
+  shareLinks: [
+    { name: "whatsapp", url: "https://wa.me/?text=" },
+    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+    { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "telegram", url: "https://t.me/share/url?url=" },
+    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
