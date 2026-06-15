851bab0ee84f00d14aeaad440be1c57ae922832c
nebuscar <3014382844@qq.com>
2026-06-15 10:59:59 +0800

    Add wallpaper and theme appearance settings

---
+++ src/config/backgroundWallpaper.ts
@@ -0,0 +1,9 @@
+export type WallpaperMode = "banner" | "fullscreen" | "overlay" | "none";
+
+export const backgroundWallpaper = {
+  defaultMode: "banner" as WallpaperMode,
+  defaultHue: 202,
+  desktop: "https://t.alcy.cc/pc",
+  mobile: "https://t.alcy.cc/mp",
+  bannerHeight: "38svh",
+};
