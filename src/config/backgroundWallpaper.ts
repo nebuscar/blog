041a4941a20cd9485d6b0328c2bb7b26c454cb94
nebuscar <3014382844@qq.com>
2026-06-15 11:12:23 +0800

    Refine fullscreen and banner wallpaper modes

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -1,9 +1,8 @@
-export type WallpaperMode = "banner" | "fullscreen" | "overlay" | "none";
+export type WallpaperMode = "banner" | "fullscreen" | "none";
 
 export const backgroundWallpaper = {
   defaultMode: "banner" as WallpaperMode,
   defaultHue: 202,
   desktop: "https://t.alcy.cc/pc",
   mobile: "https://t.alcy.cc/mp",
-  bannerHeight: "38svh",
 };
