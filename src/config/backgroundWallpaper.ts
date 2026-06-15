c4a434ac9f5d4b960d12ce1fd62d73c730ba8702
nebuscar <3014382844@qq.com>
2026-06-15 12:13:24 +0800

    Keep fullscreen wallpaper as the only mode

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -1,7 +1,4 @@
-export type WallpaperMode = "banner" | "fullscreen" | "none";
-
 export const backgroundWallpaper = {
-  defaultMode: "banner" as WallpaperMode,
   defaultHue: 202,
   desktop: "https://t.alcy.cc/pc",
   mobile: "https://t.alcy.cc/mp",
